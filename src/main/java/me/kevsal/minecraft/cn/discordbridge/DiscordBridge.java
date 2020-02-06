b1044deb8ac818284c2a3ed41f47d5716a74f1ce
Kevinsal03 <kevinsal03@users.noreply.github.com>
2020-02-06 17:12:40 -0500

    Default Init

---
+++ src/main/java/me/kevsal/minecraft/cn/discordbridge/DiscordBridge.java
@@ -0,0 +1,11 @@
+package me.kevsal.minecraft.cn.discordbridge;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class DiscordBridge extends Plugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
